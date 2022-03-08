b883563763bf7676b5769a53f520aa26c7f276ea
fatihsamur <a.fatihsamur@gmail.com>
2022-03-09 00:27:26 +0700

    maint-component-structures

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import SingleProductCard from './SingleProductCard';
+
+const MainContent = () => {
+  return (
+    <div>
+      <section>
+        <div class="max-w-screen-xl px-4 py-8 mx-auto">
+          <div class="grid grid-cols-2 mt-8 lg:grid-cols-4 gap-x-4 gap-y-8">
+            <SingleProductCard />
+            <SingleProductCard />
+            <SingleProductCard />
+            <SingleProductCard />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default MainContent;
